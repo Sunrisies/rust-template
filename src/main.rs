13d11d7e91b3c887e3403a6c66da9f902fbb94e5
朱重迁 <z32664206862022@163.com>
2025-08-26 15:55:27 +0800

    project init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// pub fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
